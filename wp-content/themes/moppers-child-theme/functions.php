daecf3d11a700fe9c3d70f02aa822eff83a93ccb
svirom <svirom@yahoo.com>
2019-07-18 17:03:40 +0300

    work edits

--- wp-content/themes/moppers-child-theme/functions.php
+++ wp-content/themes/moppers-child-theme/functions.php
@@ -1,19 +1,51 @@
 <?php
+// Prevent direct script access
+if ( !defined( 'ABSPATH' ) )
+  die ( 'No direct script access allowed' );
+  
 /**
  * SviRom
  * Moppers Child Theme
  *
  **/
 
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
- 
-  $parent_style = 'parent-style'; // This is 'moppers-style' for the Moppers theme.
-
-  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style',
-      get_stylesheet_directory_uri() . '/style.css',
-      array( $parent_style ),
-      wp_get_theme()->get('Version')
-  );
+/* Add child theme styles */
+add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
+function enqueue_child_theme_styles() {
+  wp_enqueue_style( 'moppers-child-style', get_stylesheet_directory_uri().'/style.css' );
 }
+
+/* Remove GET param ver from js/css */
+function remove_cssjs_ver( $src ) {
+	$parts = explode( '?ver', $src );
+	return $parts[0];
+}
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 15, 1 );
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 15, 1 );
+
+/* Stop loading wp-emoji-release.min.js in */
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+/* remove unnecessary code in header */
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+
+add_filter('the_generator', function ()
+{
+	return '';
+});
+
+/* remove REST API output in header */
+function remove_api () {
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+}
+add_action( 'after_setup_theme', 'remove_api' );
